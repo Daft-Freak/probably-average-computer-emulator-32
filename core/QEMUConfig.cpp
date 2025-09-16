2a7d7f947cbc5c49054e7199ccfdf2258aa39c6c
Charlie Birks <charlie@daft.games>
2025-09-16 15:44:08 +0100

    Implement enough of the QEMU config interface to get seabios to load a VGA BIOS

---
+++ core/QEMUConfig.cpp
@@ -0,0 +1,66 @@
+#include <cstdio>
+
+#include "QEMUConfig.h"
+
+struct QEMUConfigFile
+{
+    uint32_t size;
+    uint16_t index;
+    uint16_t pad;
+    char name[56];
+};
+
+static const QEMUConfigFile files[]{
+    __builtin_bswap32(35 * 1024), __builtin_bswap16(0x20), 0, "vgaroms/vgabios.bin"
+};
+static const uint32_t numFiles = __builtin_bswap32(std::size(files));
+
+QEMUConfig::QEMUConfig(System &sys)
+{
+    sys.addIODevice(0xFFFE, 0x510, 0, this);
+}
+
+void QEMUConfig::setVGABIOS(const uint8_t *bios)
+{
+    vgaBIOS = bios;    
+}
+
+uint8_t QEMUConfig::read(uint16_t addr)
+{
+    if(addr & 1) // 511 (data)
+    {
+        if(index == 0x0) // signature
+        {
+            if(dataOffset < 4)
+                return "QEMU"[dataOffset++];
+        }
+        else if(index == 0x1) // id
+            return 1;
+        else if(index == 0x19) // files
+        {
+            // first return the count
+            if(dataOffset < 4)
+                return numFiles >> (8 * dataOffset++);
+            // then the list
+            else if(dataOffset < sizeof(files) + 4)
+                return reinterpret_cast<const uint8_t *>(files)[(dataOffset++) - 4];
+        }
+        else if(index == 0x20) // vga bios file
+            return vgaBIOS ? vgaBIOS[dataOffset++] : 0;
+        else
+            printf("QEMU CFG %x read\n", index);
+    }
+
+    return 0;
+}
+
+void QEMUConfig::write(uint16_t addr, uint8_t data)
+{
+    if(addr & 1)
+        index |= data << 8;
+    else
+    {
+        index = data;
+        dataOffset = 0;
+    }
+}
---
+++ core/QEMUConfig.h
@@ -0,0 +1,26 @@
+#include "System.h"
+
+// currently only used to get seabios to load the vga bios
+class QEMUConfig : public IODevice
+{
+public:
+    QEMUConfig(System &sys);
+
+    void setVGABIOS(const uint8_t *bios);
+
+    uint8_t read(uint16_t addr) override;
+    void write(uint16_t addr, uint8_t data) override;
+
+    void updateForInterrupts(uint8_t mask) override {}
+    int getCyclesToNextInterrupt(uint32_t cycleCount) override {return 0;}
+
+    uint8_t dmaRead(int ch) override {return 0xFF;}
+    void dmaWrite(int ch, uint8_t data) override {}
+    void dmaComplete(int ch) override {}
+
+private:
+    uint16_t index;
+    uint32_t dataOffset;
+
+    const uint8_t *vgaBIOS;
+};
