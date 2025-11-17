494e7ff2e6e6e668d88ca30d02d4b9ea6d72f867
Charlie Birks <charlie@daft.games>
2025-11-17 13:32:29 +0000

    Some horrible ESP32 timer hacking
    Up to 2x faster in benchmarks. Hopefully at some point I'll improve the PIT interrupts to not require checking the timer so frequently

--- core/System.h
+++ core/System.h
@@ -11,7 +11,14 @@
 #include "hardware/pio.h"
 #endif
 #ifdef ESP_BUILD
+#define LL_TIMER // hack to significantly reduce overhead
 #include "driver/gptimer.h"
+
+#ifdef LL_TIMER
+#define _Atomic
+#include "../src/gptimer_priv.h" // gptimer_t
+#include "hal/timer_ll.h"
+#endif
 #endif
 
 class System;
@@ -215,7 +222,13 @@ public:
 #elif defined(ESP_BUILD)
         extern gptimer_handle_t sysTimer;
         uint64_t count;
+#ifdef LL_TIMER
+        auto context = &sysTimer->hal;
+        timer_ll_trigger_soft_capture(context->dev, context->timer_id);
+        count = timer_ll_get_counter_value(context->dev, context->timer_id);
+#else
         gptimer_get_raw_count(sysTimer, &count);
+#endif
         return count << 2;
 #else
         return cycleCount;
