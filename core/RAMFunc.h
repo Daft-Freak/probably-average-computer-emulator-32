fc866367b6bf85cfa90576e267d3586571a70def
Charlie Birks <charlie@daft.games>
2026-02-23 16:31:08 +0000

    Support RAM_FUNC in ESP32 build
    Doesn't seem to improve anything though

--- core/RAMFunc.h
+++ core/RAMFunc.h
@@ -2,6 +2,9 @@
 #ifdef PICO_BUILD
 #include "pico.h"
 #define RAM_FUNC(x) __not_in_flash_func(x)
+#elif defined(ESP_BUILD)
+#include "esp_attr.h"
+#define RAM_FUNC(x) IRAM_ATTR x
 #else
 #define RAM_FUNC(x) x
 #endif
